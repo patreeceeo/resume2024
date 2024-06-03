2412ab9015af8535060dccef547d5a103f3d497f
Patrick Canfield <pscale01@gmail.com>
2024-06-02 19:04:38 -0700

    redesign

--- src/main.ts
+++ src/main.ts
@@ -4,54 +4,35 @@ import data from './data.json'
 declare const divExperience: HTMLElement;
 
 // TODO use other fields
-for(const {company, location, position, start, end, description} of data.experience) {
+for(const {company, location, position, start, end, description, employmentType, remote, skills} of data.experience) {
   const div = document.createElement('div')
   div.innerHTML = [
-    `<h3><b>${company},</b> ${location} &mdash; <i>${position}</i></h3>`,
+    `<h3><b>${company},</b> ${location} &mdash; <i>${position}</i> (${ remote ? "remote " : ""}${employmentType})</h3>`,
     `<date>${start && end ? `${start} &mdash; ${end}` : start ? `${start} &mdash; PRESENT` : ''}</date>`,
-    description ? `<div class="list">${description!.map(d => `<div class="list-item">${d}</div>`).join('')}</div>` : ''
+    renderSkills(skills),
+    description ? `<div class="list">${description!.map(d => `<div class="list-item">${d}</div>`).join('')}</div>` : '',
   ].join('')
   divExperience.appendChild(div)
 }
 
 declare const divEducation: HTMLElement;
-for(const {school, location, degree, start, end, description} of data.education) {
+for(const {school, location, degree, start, end, description, skills} of data.education) {
   const div = document.createElement('div')
   div.innerHTML = [
     `<h3><b>${school},</b> ${location} &mdash; <i>${degree}</i></h3>`,
     `<date>${start && end ? `${start} &mdash; ${end}` : start ? `${start} &mdash; PRESENT` : ''}</date>`,
-    description ? `<div class="list">${description!.map(d => `<div class="list-item">${d}</div>`).join('')}</div>` : ''
+    renderSkills(skills),
+    description ? `<div class="list">${description!.map(d => `<div class="list-item">${d}</div>`).join('')}</div>` : '',
   ].join('')
   divEducation.appendChild(div)
 }
 
-declare const divSkills: HTMLElement;
-
-function renderSkill(name: string, type: 'programming' | 'tool' | 'language' | 'concept' | 'soft') {
-  const el = document.createElement('span')
-  el.className = 'skill skill-' + type
-  el.textContent = name
-  divSkills.appendChild(el)
-}
-
-for(const name of data.skillProgramming) {
-  renderSkill(`${name}`, 'programming')
-}
-
-for(const name of data.skillTools) {
-  renderSkill(`${name}`, 'tool')
-}
-
-for(const name of data.skillConcepts) {
-  renderSkill(`${name}`, 'concept')
-}
-
-for(const name of data.skillSoft) {
-  renderSkill(`${name}`, 'soft')
-}
-
-for(const name of data.skillLanguages) {
-  renderSkill(`${name}`, 'language')
+function renderSkills(skills: string[]): string {
+  const results = []
+  for(const skill of skills) {
+    results.push(`<span class="skill">${skill}</span>`)
+  }
+  return results.join("")
 }
 
 declare const divProjects: HTMLElement;
